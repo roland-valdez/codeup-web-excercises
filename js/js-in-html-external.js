454370ce339c41fe07d10d603208842c12c655ed
Roland Valdez <valdez.roland.23@gmail.com>
2021-02-02 11:17:53 -0600

    feat: added message windows to external js

--- js/js-in-html-external.js
+++ js/js-in-html-external.js
@@ -1,5 +1,16 @@
 "use strict"
 console.log("Hello from an external JS file.");
 
-name = "Douglas";
+var name = "Douglas";
 console.log("Hello " + name);
+
+//Alert
+alert("Welcome to my site!");
+
+//Confirm
+var confirmLeave = confirm("Are you sure you want to leave 0.0");
+console.log(confirmLeave);
+
+//Prompt
+var favoriteColor = prompt("What is your favorite color");
+console.log("Your favorite color is " + favoriteColor);
