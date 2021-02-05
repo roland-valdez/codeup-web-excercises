318b6239a12bb25eaecc84acf1b3e8713ecc487b
Roland Valdez <valdez.roland.23@gmail.com>
2021-02-05 09:34:19 -0600

    adding finished project on simple function drills

---
+++ js/simple_function_drills.js
@@ -0,0 +1,37 @@
+"use strict"
+
+// 1. Make a function called returnTwo() that returns the number 2 when called
+// 2. Test this function with console.log(returnTwo())
+function returnTwo(){
+    return 2;
+}
+console.log(returnTwo());
+
+// 3. Make a function called sayHowdy() which console.logs the string “Howdy!”
+// 4. Test this function by directly calling sayHowdy()
+// 5. Remember this function does not need a defined return value
+function sayHowdy(){
+    return "Howdy";
+}
+console.log(sayHowdy());
+
+// 6. Make a function called returnName() that returns the string of your name
+// 7. Test this function with console.log(returnName())
+function returnName(){
+    return "Roland"
+}
+console.log(returnName());
+
+// 8. Make a function called addThree() which takes in a number input and returns the number plus 3.
+// 9. Test this function with console.log(addThree(5))
+function addThree(number){
+    return number + 3;
+}
+console.log(addThree(5));
+
+// 10. Make a function called sayString() which returns the string input passed in.
+// 11.Test this function with console.log(sayString('codeup'))
+function sayString(string){
+    return "codeup";
+}
+console.log(sayString('codeup'))
