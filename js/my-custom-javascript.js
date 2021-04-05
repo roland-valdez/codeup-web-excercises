44109dd8cd874c1bf12ae403f176045f60a51cd9
Roland Valdez <valdez.roland.23@gmail.com>
2021-04-05 17:01:24 -0500

    finished jQuery selectors

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,14 @@
 "use strict";
 
 $(document).ready(function() {
-    alert("DOM has finished loading")
+  // $('.codeup').css('border', '1px');
+  //   $('.codeup').css('border', '5px yellow');
+  //   $('.codeup').css('border', '3px solid red');
+  //   $('#codeup').css('border', '3px solid red')
+  //   $('li').css('background', 'red');
+  //   // $('h1').css('background', 'red');
+  //   $('p').css('background', 'red');
+  //   var statement = $('h4').html();
+  //   alert(statement);
+    $('h4, p, li').css('background', 'orange');
 });
