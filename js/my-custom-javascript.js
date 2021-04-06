8884e59fb708facea752256be2f49627f333bf9b
Roland Valdez <valdez.roland.23@gmail.com>
2021-04-06 16:12:49 -0500

    konami code draft

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $('h1').click(function(){
         $(this).css('background-color', 'rebeccapurple');
     });
-    $('p').dblclick(function(){
+    $('p').click(function(){
         $(this).css('font-size', '30px');
     });
     $('li').hover(function(){
@@ -12,4 +12,22 @@ $(document).ready(function() {
     }, function(){
         $(this).css('color', 'black');
     })
+    $('p').dblclick(function(){
+        $(this).css('display', 'none');
+    })
+    $('.btn').click(function(){
+        $(this).html("Howdy!")
+    })
+    var hoverIn = function(e){
+        var htmlCont = $(e.target).html();
+        $(e.target).html(htmlCont.toUpperCase());
+    };
+    // var hoverOut = function(e){
+    //     var htmlCont = $(e.target).html();
+    //     $(e.target).html(htmlCont.toLowerCase());
+    // }
+    $('p').hover(hoverIn, function(e){
+        var htmlCont = $(e.target).html();
+        $(e.target).html(htmlCont.toLowerCase());
+    });
 });
